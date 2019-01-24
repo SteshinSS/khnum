29b5fafda98f8e5b525970258e693f9ea7a0be49
Repayment <poft@yandex.ru>
2019-01-24 15:32:09 +0300

    Finish MID Convolution

--- src/utilities/MID.cpp
+++ src/utilities/MID.cpp
@@ -7,7 +7,11 @@ MID operator*(const MID &lhs, const MID &rhs) {
     for (int mass_shift = 0; mass_shift < convolve_result.size(); ++mass_shift) {
         for (int lhs_mass_shift = 0; lhs_mass_shift < lhs.size(); ++lhs_mass_shift) {
             int rhs_mass_shift = mass_shift - lhs_mass_shift;
-            if (rhs_mass_shift < 0 || rhs_mass_shift > rhs.size())
+            if (rhs_mass_shift >= 0 || rhs_mass_shift < rhs.size()) {
+                convolve_result[mass_shift] += lhs[lhs_mass_shift] * rhs[rhs_mass_shift];
+            }
         }
     }
+
+    return convolve_result;
 }
